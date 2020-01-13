84e1f43b3bcd043795302d86cbd476c38aac14b3
Simon Sorensen <teamangryghast@gmail.com>
2020-01-13 12:20:47 +0000

    Added timber logging

--- blueagent/logger.py
+++ blueagent/logger.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.INFO)
 
 logger.addHandler(logging.StreamHandler())
 
-# timber_handler = timber.TimberHandler(api_key=os.environ['TIMBER_API_KEY'])
-# logger.addHandler(timber_handler)
+timber_handler = timber.TimberHandler(source_id=os.environ['TIMBER_SOURCE_ID'], api_key=os.environ['TIMBER_API_KEY'])
+logger.addHandler(timber_handler)
 
 
